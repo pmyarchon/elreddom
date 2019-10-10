5c474ff60f866b40e24fb70806aac715f585ea77
Maxim Pushkar <archon@mail.ru>
2019-10-10 22:25:01 +0300

    Prettify router code and apply JSONHeaderPlug

--- lib/elreddom/router.ex
+++ lib/elreddom/router.ex
@@ -7,50 +7,49 @@ defmodule Router do
     pass: ["application/json"],
     json_decoder: Jason
 
+  plug JSONHeaderPlug
   plug(:match)
   plug(:dispatch)
 
   # Submit links
   post "/visited_links" do
-    {status, body} = case conn.body_params do
+    {status, result} = case conn.body_params do
       %{"links" => links} when is_list(links) ->
         now = Utils.timestamp()
 
         domains = for link <- links do
           {:ok, d} = Utils.domain(link)
-          d
+          String.downcase(d)
         end
 
         case Utils.Redis.write_domains(now, Enum.uniq(domains)) do
-          result when is_list(result) -> {200, Jason.encode!(%{status: "ok"})}
-          _ -> {500, Jason.encode!(%{status: "write_failed"})}
+          result when is_list(result) -> {201, %{status: "ok"}}
+          _ -> {500, %{status: "write_failed"}}
         end
 
-        _ ->
-          {400, Jason.encode!(%{status: "malformed_request"})}
+      _ -> {400, %{status: "malformed_request"}}
     end
 
-    send_resp(conn, status, body)
+    send_resp(conn, status, Jason.encode!(result))
   end
 
   # Get unique domains
   get "/visited_domains" do
-    {status, body} = case conn.query_params do
+    {status, result} = case conn.query_params do
       %{"from" => from, "to" => to} ->
         case Utils.Redis.get_domains(from, to) do
-          domains when is_list(domains) -> {200, Jason.encode!(%{status: "ok", domains: domains})}
-          _ -> {500, Jason.encode!(%{status: "read_failed"})}
+          domains when is_list(domains) -> {200, %{status: "ok", domains: domains}}
+          _ -> {500, %{status: "read_failed"}}
         end
 
-        _ ->
-          {400, Jason.encode!(%{status: "malformed_request"})}
-      end
-    send_resp(conn, status, body)
+      _ -> {400, %{status: "malformed_request"}}
+    end
+
+    send_resp(conn, status, Jason.encode!(result))
   end
 
   # Catch-up
   match _ do
-    send_resp(conn, 404, "Not found")
+    send_resp(conn, 404, Jason.encode!(%{status: "not_found"}))
   end
 end
-
